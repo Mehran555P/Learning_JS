6b5d73b754fff83dfd87f274f070e7a1be24d11a
Mehran Hakimnia <hakimniamehran@gmail.com>
2026-06-27 23:07:27 +0330

    Add primitives

---
+++ primitives/index.js
@@ -0,0 +1,19 @@
+// Primitives
+// #################3
+
+// String
+let name = 'Mehran';
+
+// Number
+let age = 18;
+
+// Boolean (True/False)
+let isValid = true;
+let isExist = false;
+
+// Undefined
+let something;
+let somethingElse = undefined;
+
+// null
+let selected = null;
