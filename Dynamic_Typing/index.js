c4db6a7a56bff2be7e4aef3d4ee43ac2f50ac5d9
Mehran Hakimnia <hakimniamehran@gmail.com>
2026-06-29 00:49:18 +0330

    Add Dynamic_Types

---
+++ Dynamic_Typing/index.js
@@ -0,0 +1,18 @@
+// Dynamic typing
+
+// String
+let name = 'Mehran';
+
+// Number
+let age = 18;
+
+// Boolean (True/False)
+let isValid = true;
+let isExist = false;
+
+// Undefined
+let something;
+let somethingElse = undefined;
+
+// null
+let selected = null;
