59c46e8a471e7dcc130150767bc8f40e32e0497e
Mehran Hakimnia <hakimniamehran@gmail.com>
2026-06-29 00:50:27 +0330

    Add Reference_Types

---
+++ Reference_Types/index.js
@@ -0,0 +1,16 @@
+// References types are Objects, Arrays and Functions.
+
+// Object
+
+let person = {
+    name: "Mehran",
+    age: 18
+};
+
+// Dot Notation
+person.name = 'something else';
+
+// Bracket Notation
+person['name'] = 'something else';
+
+console.log(person.name)
