fcf8b0370787acbd33d276a25df4a6329d0f1f54
Mehran Hakimnia <hakimniamehran@gmail.com>
2026-06-29 01:49:00 +0330

    Add Arrays

---
+++ Arrays/index.js
@@ -0,0 +1,12 @@
+// Arrays concept
+
+let colors = ['red', 'green', 'blue'];
+console.log(colors);
+console.log(colors[0]);
+console.log(colors[1]);
+console.log(colors[2]);
+
+colors[2] = 33;
+console.log(colors);
+
+console.log(colors.length);
