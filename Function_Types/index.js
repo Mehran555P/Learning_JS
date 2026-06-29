b2cc88682e62fb690cb00161a651e55355443563
Mehran Hakimnia <hakimniamehran@gmail.com>
2026-06-30 02:30:07 +0330

    Add Function_Types

---
+++ Function_Types/index.js
@@ -0,0 +1,28 @@
+// Functions
+
+function hallo() {
+    console.log('Hello world');
+}
+
+function hello(name) {
+    console.log('Hello ' + name);
+}
+
+
+
+// Performing a task
+
+function fullName(firstName, lastName) {
+    console.log('Hello ' + firstName + ' ' + lastName);
+}
+
+
+// Calculating a value
+
+function square(number) {
+    return number * number;
+}
+
+let squareN = square(2);
+console.log(squareN);
+console.log(square(2));
