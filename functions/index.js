40ce31fa989135d03ee082115d9c50969f56eab3
Mehran Hakimnia <hakimniamehran@gmail.com>
2026-06-29 20:31:24 +0330

    Add Functions

---
+++ functions/index.js
@@ -0,0 +1,19 @@
+// Functions
+
+function hallo() {
+    console.log('Hello world');
+}
+
+hallo();
+
+function hello(name) {
+    console.log('Hello ' + name);
+}
+
+hello('Mehran');
+
+function fullName(firstName, lastName) {
+    console.log('Hello ' + firstName + ' ' + lastName);
+}
+
+fullName('Mehran', 'Hakimnia');
