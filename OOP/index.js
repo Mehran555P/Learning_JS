51cd7f5b2697d3fe4d260f972ca441d0ec6b0978
Mehran Hakimnia <hakimniamehran@gmail.com>
2026-07-11 15:10:30 +0330

    defining an object

--- OOP/index.js
+++ OOP/index.js
@@ -1,16 +1,43 @@
-let employee = {
-        baseSalary: 30000,
-        overTime: 10,
-        rate: 20,
-        getWage: function() {
-            return this.baseSalary + (this.overTime * this.rate);
-        }
+// Factories
 
+
+
+///////////////////////////////////////////////////////
+
+const circle = {
+    radios: 1,
+    location: {
+        x: 1,
+        y: 1
+    },
+    draw: function() {
+        console.log("draw")
+    }
 };
 
-const wage = employee.getWage();
-console.log(wage);
+circle.draw();
 
-console.log(employee.baseSalary);
-console.log(employee.overTime);
-console.log(employee.rate);
+
+
+
+///////////////////////////////////////////////////////
+
+
+// let employee = {
+//         baseSalary: 30000,
+//         overTime: 10,
+//         rate: 20,
+//         getWage: function() {
+//             return this.baseSalary + (this.overTime * this.rate);
+//         }
+
+// };
+
+// const wage = employee.getWage();
+// console.log(wage);
+
+// console.log(employee.baseSalary);
+// console.log(employee.overTime);
+// console.log(employee.rate);
+
+// console.log(typeof(employee));
