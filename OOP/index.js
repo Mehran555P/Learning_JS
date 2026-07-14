a4a793ecf66f54bc44bdf64bedb73b43ff7c4262
Mehran Hakimnia <hakimniamehran@gmail.com>
2026-07-14 23:24:36 +0330

    Primitives and reference types

--- OOP/index.js
+++ OOP/index.js
@@ -1,20 +1,44 @@
+///////////////////////////////////////////////////////
+
+// // Value(primitives) vs references types
+
+// // primitives are copied by their value
+// let x = 10;
+// let y = x;
+// x = 20;
+
+
+// // Objects are copied by their reference
+// let x = {value: 10};
+// let y = x;
+// x.value = 20;
+
+let obj = {value: 10};
+
+function increase(obj) {
+    obj.value++;
+}
+
+increase(obj);
+console.log(obj.value);
+
 
 ///////////////////////////////////////////////////////
 
 // // Functions are Objects in JS!
 
-function Circle(radius) {
-    this.radius = radius;
-    this.draw = function() {
-        console.log("draw");
-    }
-}
+// function Circle(radius) {
+//     this.radius = radius;
+//     this.draw = function() {
+//         console.log("draw");
+//     }
+// }
 
-Circle.call({}, 1);
-Circle.apply({}, [1]);
+// Circle.call({}, 1);
+// Circle.apply({}, [1]);
 
 
-const another = new Circle(1);
+// const another = new Circle(1);
 
 // const circle = new Circle1(1);
 
