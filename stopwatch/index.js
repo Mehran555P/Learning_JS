d6f8cc8bbcebe9a4f77f31a512cef934c1614900
Mehran Hakimnia <hakimniamehran@gmail.com>
2026-07-20 14:22:58 +0330

    Add stopwatch program

---
+++ stopwatch/index.js
@@ -0,0 +1,20 @@
+
+function Stopwatch() {
+    let startTime, stopTime, running, duration = 0;
+
+    this.start = function() {
+        //
+    }
+
+    this.stop = function() {
+        //
+    }
+
+    this.reset = function() {
+        //
+    }
+
+    Object.defineProperty(this, 'duration', {
+
+    }); 
+}
