2030d3441a58c440c85f887cdffc22417bd78add
Mehran Hakimnia <hakimniamehran@gmail.com>
2026-07-20 19:44:26 +0330

    end commit of stopwatch program

--- stopwatch/index.js
+++ stopwatch/index.js
@@ -3,18 +3,35 @@ function Stopwatch() {
     let startTime, stopTime, running, duration = 0;
 
     this.start = function() {
-        //
+        if (running) 
+            throw new Error("the stopwatch is already run.")
+
+        running = true;
+        startTime = new Date();
+    
     }
 
     this.stop = function() {
-        //
+        if (!running) 
+            throw new Error("the stopwatch is already stoped.")
+        
+        running = false;
+        stopTime = new Date();
+
+        const seconds = ( stopTime.getTime() - startTime.getTime() ) / 1000;
+        duration += seconds;
     }
 
     this.reset = function() {
-        //
+        startTime = null;
+        stopTime = null;
+        running = false;
+        duration = 0;
     }
 
     Object.defineProperty(this, 'duration', {
-
+        get: function() {
+            return duration;
+        }
     }); 
 }
